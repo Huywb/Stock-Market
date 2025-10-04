a649c425472cf281670f576594a6c3dfc7e902e6
huywb <pthhuy1306@gmail.com>
2025-10-04 09:56:11 +0700

    Menu Item header

---
+++ lib/contant.ts
@@ -0,0 +1,15 @@
+
+export const NAV_ITEMS = [
+    {
+        href: '/',
+        title: 'Dashboard'
+    },
+    {
+        href: '/search',
+        title: 'Search'
+    },
+    {
+        href: '/watchlist',
+        title: 'Watchlist'
+    }
+]
