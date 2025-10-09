3c0ea4a2cbc5324ab7816866bf5e7079e262cd39
huywb <pthhuy1306@gmail.com>
2025-10-09 09:50:34 +0700

    Sign-up Layout and UI

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,122 @@
+'use client'
+import CountrySelect from '@/components/form/CountrySelect'
+import FooterLink from '@/components/form/FooterLink'
+import InputField from '@/components/form/InputField'
+import SelectField from '@/components/form/SelectField'
+import { Button } from '@/components/ui/button'
+import { INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS } from '@/lib/contant'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+
+const SignUp = () => {
+
+    const {register,handleSubmit,control,
+        formState: {errors,isSubmitting}
+    } = useForm<SignUpFormData>({
+        defaultValues:{
+            fullName: "",
+            email: "",
+            password: "",
+            country: "VN",
+            investmentGoals: "Growth",
+            riskTolerance: "Medium",
+            preferredIndustry: "Technology"
+        },
+        mode: 'onBlur'
+    })  
+
+    const onSubmit =async (data: SignUpFormData)=>{
+        try {
+            console.log(data)
+        } catch (error) {
+            console.error(0)
+        }
+    }
+
+    return (
+    <>
+        <h1 className='form-title'>Sign Up & Personalize</h1>
+        
+        <form onSubmit={handleSubmit(onSubmit)}     className='space-y-5'>
+            {/**Input */}
+
+            <InputField 
+                name='fullName' 
+                label='Full Name' 
+                placeholder="Huy Pham"
+                register ={register}
+                error={errors.fullName}
+                validation={{required: "Full name is required",minLength: 2}} 
+            />
+
+            <InputField 
+                name='email' 
+                label='Email' 
+                placeholder="Huypham@gmail.com"
+                register ={register}
+                error={errors.email}
+                validation={{required: "Email is required",
+                    pattern:"^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$",
+                    message:"Email adress is required"
+                }} 
+            />
+
+            <InputField 
+                name='password' 
+                label='Password' 
+                placeholder="Enter a strong password"
+                type='password'
+                register ={register}
+                error={errors.password}
+                validation={{required: "Password is required",minLength: 8}} 
+            />
+
+            <CountrySelect 
+                name='Country'
+                label='Country'
+                control={control}
+                error={errors.country}
+                required
+            />
+           
+           <SelectField 
+                name = 'investmentGoals'
+                label = 'Investment Goals'
+                placeholder = 'Select your investment goal'
+                options = {INVESTMENT_GOALS}
+                control = {control}
+                error = {errors.investmentGoals}
+                required                
+            />
+
+            <SelectField 
+                name = 'riskTolerance'
+                label = 'Risk Tolerance'
+                placeholder = 'Select your risk level'
+                options = {RISK_TOLERANCE_OPTIONS}
+                control = {control}
+                error = {errors.riskTolerance}
+                required                
+            />
+
+            <SelectField 
+                name = 'preferredIndustry'
+                label = 'Prefered Industry'
+                placeholder = 'Select your preferred industry'
+                options = {PREFERRED_INDUSTRIES}
+                control = {control}
+                error = {errors.investmentGoals}
+                required                
+            />
+
+            <Button type='submit' disabled={isSubmitting} className='yellow-btn w-full mt-5'>
+                {isSubmitting ? 'Creating Account' : 'Star Your Investing Journey'}
+            </Button>
+
+            <FooterLink text='Already have an account' linkText='Sign in' href='/sign-in'></FooterLink>
+        </form>
+    </>
+  )
+}
+
+export default SignUp
--- components/form/CountrySelect.tsx
+++ components/form/CountrySelect.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import {
   Popover,
   PopoverContent,
@@ -18,7 +19,22 @@ import {
 } from "@/components/ui/command"
 import { Label } from '../ui/label'
 import { Controller } from 'react-hook-form'
+import countryList from 'react-select-country-list'
+import { Button } from '../ui/button'
 const CountrySelect = ({name, label , control, required = false, error}:CountrySelectProps) => {
+  
+  const countries = countryList().getData()  
+  const [open, setOpen] = useState(false)
+  const [localValue, setLocalValue] = useState<string | null>(null)
+
+  const getFlagEmoji = (countryCode: string) => {
+        const codePoints = countryCode
+            .toUpperCase()
+            .split('')
+            .map((char) => 127397 + char.charCodeAt(0));
+        return String.fromCodePoint(...codePoints);
+    };
+
   return (
     <div className='space-y-2'>
         <Label htmlFor={name} className='form-label'>
@@ -30,25 +46,35 @@ const CountrySelect = ({name, label , control, required = false, error}:CountryS
             rules={{
                 required: required ? `Please select ${label.toLowerCase()}` : false,
             }}
-            render={({field})=>(
-                <Popover>
-                    <PopoverTrigger className='country-select-trigger'>Choose your country</PopoverTrigger>
-                    <PopoverContent className='country-select-input '>
-                        <Command className='country-select-input '>
-                            <CommandInput placeholder="Type a command or search..." />
-                            <CommandList>
-                                <CommandEmpty>No results found.</CommandEmpty>
-                                <CommandGroup heading="Suggestions">
-                                <CommandItem>USA</CommandItem>
-                                <CommandItem>Viet Nam</CommandItem>
-                                <CommandItem>China</CommandItem>
-                                </CommandGroup>
+            render={({field})=>{
+                const displayvalue = field.value ?? localValue
+                return (
+                <Popover open={open} onOpenChange={setOpen} >
+                    <PopoverTrigger asChild className=''>
+                      <Button variant='ghost' className='country-select-trigger'>
+                        {displayvalue ? displayvalue : "Choose your country"}
+                      </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className='w-full p-0'  >
+                        <Command className='country-select-input scrollbar-hide-default w-full '>
+                            <CommandInput  placeholder="Type a command or search..." />
+                            <CommandList className='max-h-60 '>
+                                <CommandEmpty>No country found.</CommandEmpty>
+                                {countries.map(country=>(
+                                <CommandItem key={country.value} 
+                                onSelect={()=>{ 
+                                            field.onChange(country.label)
+                                            setLocalValue(country.label)
+                                            setOpen(false)
+                                }}>{getFlagEmoji(country.value)} {country.label}</CommandItem>
+                                ))}
                             </CommandList>
                         </Command>
                     </PopoverContent>
                 </Popover>
-            )}
+            )}}
       />
+      {error && <p className='text-sm text-red-500'>{error.message}</p>}
     </div>
   )
 }
